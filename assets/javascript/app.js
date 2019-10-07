7a5ecf950464c78663a21a50af72136f984c30d2
Chingford <chingford162@outlook.com>
2019-10-07 15:43:50 -0400

    working version, doesn't prepend or add new animals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,12 +12,14 @@ xhr.done(function(data) { console.log("success got data", data); }); this is the
 
 //copy past AJAX CALL from website
 
-var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=UeWz8rShEHWfhx6NbamSEfp89pmQZ44r&limit=10");
+/*var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=UeWz8rShEHWfhx6NbamSEfp89pmQZ44r&limit=10");
 
 xhr.done(function (data) {
   console.log("success got data", data);
   showGifs(data);
 });
+
+//shows giphs
 function showGifs(gifInfo){
   var giphs = gifInfo.data;
   console.log(giphs);
@@ -25,26 +27,34 @@ function showGifs(gifInfo){
     $('.container').append("<img src= ' " + giphs[i].images.original.url + " '/>")
   };
 };
+*/
 
 
 
-/* Initial array of tech terms
-var movies = ["Variable", "Object", "Javascript", "Element"];
+// displayAnimal function re-renders the HTML to display the appropriate content
+function displayAnimal() {
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-function displayTechgiphy() {
+  //Ajax call
+  var animal = $(this).attr("data-name");
+  
+  var queryURL = $.get("https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=UeWz8rShEHWfhx6NbamSEfp89pmQZ44r&limit=10");
 
-  var tech = $(this).attr("data-name");
-  var queryURL = "https://www.omdbapi.com/?t=" + tech + "&apikey=trilogy";
+ 
+  queryURL.done(function (data) {
+    console.log("success got data", data);
+    showGifs(data);
+  });
+    
+    function showGifs(gifInfo){
+      var giphs = gifInfo.data;
+      console.log(giphs);
+      for (i in giphs) {
+        $('.container').append("<img src= ' " + giphs[i].images.original.url + " '/>")
+      };
+    };
 
-  // Creating an AJAX call for the specific movie button being clicked
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-    // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
+    // Creating a div to hold the animal
+    var animalDiv = $("<div class='animal'>");
 
     // Storing the rating data
     var rating = response.Rated;
@@ -53,81 +63,61 @@ function displayTechgiphy() {
     var pOne = $("<p>").text("Rating: " + rating);
 
     // Displaying the rating
-    movieDiv.append(pOne);
+    animalDiv.append(pOne);
 
-    // Storing the release year
-    var released = response.Released;
-
-    // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
-
-    // Displaying the release year
-    movieDiv.append(pTwo);
-
-    // Storing the plot
-    var plot = response.Plot;
-
-    // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
-
-    // Appending the plot
-    movieDiv.append(pThree);
-
-    // Retrieving the URL for the image
-    var imgURL = response.Poster;
-
-    // Creating an element to hold the image
+        // Creating an element to hold the image
     var image = $("<img>").attr("src", imgURL);
 
     // Appending the image
     movieDiv.append(image);
 
-    // Putting the entire movie above the previous movies
-    $("#movies-view").prepend(movieDiv);
-  });
+    // Putting the entire animal giph above the previous giph
+    $("#animal-view").prepend(animalDiv);
+  };
 
-}
-
-// Function for displaying movie data
+// Function for displaying animals
 function renderButtons() {
 
-  // Deleting the movies prior to adding new movies
+  // Deleting the animals prior to adding new animals
   // (this is necessary otherwise you will have repeat buttons)
   $("#buttons-view").empty();
 
+  // Initial array of movies
+var animal = ["Dog", "Hedgehog", "Cat", "Peacock"];
+
   // Looping through the array of movies
-  for (var i = 0; i < movies.length; i++) {
+  for (var i = 0; i < animal.length; i++) {
 
     // Then dynamicaly generating buttons for each movie in the array
     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
     var a = $("<button>");
-    // Adding a class of movie-btn to our button
-    a.addClass("movie-btn");
+    // Adding a class of animal-btn to our button
+    a.addClass("animal-btn");
     // Adding a data-attribute
-    a.attr("data-name", movies[i]);
+    a.attr("data-name", animal[i]);
     // Providing the initial button text
-    a.text(movies[i]);
+    a.text(animal[i]);
     // Adding the button to the buttons-view div
     $("#buttons-view").append(a);
   }
 }
 
-// This function handles events where a movie button is clicked
-$("#add-movie").on("click", function(event) {
+// This function handles events where a animal button is clicked
+$("#add-animal").on("click", function(event) {
   event.preventDefault();
+
   // This line grabs the input from the textbox
-  var movie = $("#movie-input").val().trim();
+  var animal = $("#animal-input").val().trim();
 
-  // Adding movie from the textbox to our array
-  movies.push(movie);
+  // Adding animal from the textbox to our array
+  animal.push(animal);
 
-  // Calling renderButtons which handles the processing of our movie array
+  // Calling renderButtons which handles the processing of our animal array
   renderButtons();
 });
 
 // Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".movie-btn", displayMovieInfo);
+$(document).on("click", ".animal-btn", displayAnimal);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
-*/
